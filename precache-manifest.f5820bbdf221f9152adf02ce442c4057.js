2d3378d3046d1fbe51b194d9277e33012dd09935
Wesley Chiu <violinwesley@gmail.com>
2020-04-28 16:49:56 -0400

    Updates

---
+++ precache-manifest.f5820bbdf221f9152adf02ce442c4057.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a069ce782702206a859ffa18aa07aaf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc8b092452c7de5956f6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "741759043b08510c3361",
+    "url": "/static/js/2.07dad784.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.07dad784.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8b092452c7de5956f6",
+    "url": "/static/js/main.508e49a1.chunk.js"
+  },
+  {
+    "revision": "087fe1c865747a202ce5",
+    "url": "/static/js/runtime-main.e9e1c586.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
